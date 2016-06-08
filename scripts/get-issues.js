475f3d09f6ab8d0f5a05741c96d8e8da39a25d66
Glavin Wiechert <glavin.wiechert@gmail.com>
2016-06-08 01:20:28 -0300

    See #2, #3. Retrieve Issues from GitHub, classify issue labels

---
+++ scripts/get-issues.js
@@ -0,0 +1,84 @@
+// Dependencies
+const config = require('config');
+const GitHubApi = require("github");
+const _ = require('lodash');
+const fs = require('fs');
+const path = require('path');
+const mkdirp = require('mkdirp');
+const async = require('async');
+
+// Setup client(s)
+const GitHubToken = config.get('github.token');
+const github = new GitHubApi({
+  debug: false,
+});
+// OAuth2
+github.authenticate({
+    type: "oauth",
+    token: GitHubToken
+});
+
+// ===== Helpers =======
+
+// http://davidarvelo.com/blog/array-number-range-sequences-in-javascript-es6/
+// create a generator function returning an
+// iterator to a specified range of numbers
+function* range(begin, end, interval = 1) {
+  for (let i = begin; i < end; i += interval) {
+    yield i;
+  }
+}
+
+// Get list of Issues for repository
+const githubPageSize = 100;
+let user = 'Glavin001';
+let repo = 'atom-beautify';
+let dataPath = path.resolve(__dirname, '../data/',user,repo);
+console.log('dataPath', dataPath);
+async.parallel([
+  (cb) => mkdirp(dataPath, cb),
+  (cb) => {
+    // FIXME: make this dynamic
+    let numOfIssues = 1007;
+
+    // Get all Repositories
+    let pages = range(1, parseInt(numOfIssues / githubPageSize) + 2, 1);
+    async.map(pages, (page, cb) => {
+      github.issues.getForRepo({
+        user,
+        repo,
+        page,
+        state: 'all',
+        per_page: githubPageSize,
+      }, cb);
+    }, (error, issues) => {
+      if (error) {
+        return cb(error);
+      }
+      issues = _.flatten(issues);
+      return cb(null, issues);
+    })
+  }
+], (err, results) => {
+  // Get contents for each Issue
+  // console.log(err, issues);
+  let issues = results[1];
+  issues = _.map(issues, (issue) => {
+    let labels = _.map(issue.labels, 'name');
+    return {
+      number: issue.number,
+      pull_request: issue.pull_request,
+      state: issue.state,
+      text: `${issue.title} \n ${issue.body}`,
+      labels: labels
+    };
+  });
+
+
+  console.log('Number of Issues: ',issues.length);
+  fs.writeFile(path.resolve(dataPath, 'issues.json'), JSON.stringify(issues, undefined, 4), (err) => {
+    console.log(err);
+  });
+
+});
+
--- scripts/multi-label_classification.py
+++ scripts/multi-label_classification.py
@@ -1,3 +1,4 @@
+# Dependencies
 import numpy as np
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
@@ -5,45 +6,105 @@ from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn import preprocessing
-
-X_train = np.array(["new york is a hell of a town",
-                    "new york was originally dutch",
-                    "the big apple is great",
-                    "new york is also called the big apple",
-                    "nyc is nice",
-                    "people abbreviate new york city as nyc",
-                    "the capital of great britain is london",
-                    "london is in the uk",
-                    "london is in england",
-                    "london is in great britain",
-                    "it rains a lot in london",
-                    "london hosts the british museum",
-                    "new york is great and so is london",
-                    "i like london better than new york"])
-y_train_text = [["new york"],["new york"],["new york"],["new york"],["new york"],
-                ["new york"],["london"],["london"],["london"],["london"],
-                ["london"],["london"],["new york","london"],["new york","london"]]
-
-X_test = np.array(['nice day in nyc',
-                   'welcome to london',
-                   'london is rainy',
-                   'it is raining in britian',
-                   'it is raining in britian and the big apple',
-                   'it is raining in britian and nyc',
-                   'hello welcome to new york. enjoy it here and london too'])
-target_names = ['New York', 'London']
+import json
+from pprint import pprint
 
 lb = preprocessing.MultiLabelBinarizer()
-Y = lb.fit_transform(y_train_text)
 
-classifier = Pipeline([
-    ('vectorizer', CountVectorizer()),
-    ('tfidf', TfidfTransformer()),
-    ('clf', OneVsRestClassifier(LinearSVC()))])
 
-classifier.fit(X_train, Y)
-predicted = classifier.predict(X_test)
-all_labels = lb.inverse_transform(predicted)
+def train_classifier(X_train, y_train_text):
 
-for item, labels in zip(X_test, all_labels):
-    print '%s => %s' % (item, ', '.join(labels))
+    Y = lb.fit_transform(y_train_text)
+
+    classifier = Pipeline([
+        ('vectorizer', CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        ('clf', OneVsRestClassifier(LinearSVC()))])
+
+    classifier.fit(X_train, Y)
+    return classifier
+
+def predict_with_classifier(classifier, X_test):
+    predicted = classifier.predict(X_test)
+    all_labels = lb.inverse_transform(predicted)
+
+    return zip(X_test, all_labels)
+
+def filter_labels(labels, ignore_labels):
+    for ignore in ignore_labels:
+        try:
+            labels.remove(ignore)
+        except:
+            pass
+    return labels
+
+# Get JSON data
+user = "Glavin001"
+repo = "atom-beautify"
+repoPath = './data/'+user+'/'+repo+'/'
+ignore_labels = ['duplicate', 'in-progress', 'pending-publication', 'published', 'duplicate', 'waiting-for-user-information']
+with open(repoPath+'issues.json') as data_file:
+    issues = json.load(data_file)
+
+    x_train = []
+    y_train = []
+    x_test = []
+    y_test = []
+    for issue in issues:
+        # Ignore Pull Requests
+        # if issue.has_key('pull_request'):
+        #     pass
+        labels = filter_labels(issue['labels'], ignore_labels)
+        text = issue['text'].replace("\n"," ").replace('\r', ' ')
+        if len(labels) > 0: # and issue['state'] == 'closed':
+            x_train.append(text)
+            y_train.append(labels)
+        else:
+            x_test.append(text)
+            y_test.append(labels)
+
+    # X_train = np.array(["new york is a hell of a town",
+    #                     "new york was originally dutch",
+    #                     "the big apple is great",
+    #                     "new york is also called the big apple",
+    #                     "nyc is nice",
+    #                     "people abbreviate new york city as nyc",
+    #                     "the capital of great britain is london",
+    #                     "london is in the uk",
+    #                     "london is in england",
+    #                     "london is in great britain",
+    #                     "it rains a lot in london",
+    #                     "london hosts the british museum",
+    #                     "new york is great and so is london",
+    #                     "i like london better than new york"])
+    # y_train_text = [["new york"],["new york"],["new york"],["new york"],["new york"],
+    #                 ["new york"],["london"],["london"],["london"],["london"],
+    #                 ["london"],["london"],["new york","london"],["new york","london"]]
+
+    X_train = np.array(x_train)
+    y_train_text = y_train
+
+    classifier = train_classifier(X_train, y_train_text)
+
+    # x_test = np.array(['nice day in nyc',
+    #                    'welcome to london',
+    #                    'london is rainy',
+    #                    'it is raining in britian',
+    #                    'it is raining in britian and the big apple',
+    #                    'it is raining in britian and nyc',
+    #                    'hello welcome to new york. enjoy it here and london too'])
+    # target_names = ['New York', 'London']
+
+
+    predicted = predict_with_classifier(classifier, x_test)
+
+    print(len(x_train), len(x_test))
+    results = []
+    for item, labels in predicted:
+        results.append({
+            "text": item,
+            "labels": labels
+        })
+    #     print '%s => %s' % (item, ', '.join(labels))
+    with open(repoPath+'results.json', 'w') as out_file:
+        json.dump(results, out_file, indent=4)
